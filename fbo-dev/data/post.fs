877134fdb0840d8e8156df71bba597938d1986f2
Gálffy Tamás <ezittgtx@gmail.com>
2015-05-19 22:23:03 +0200

    Modified post.fs for fun Added an actual fbo class with an untested implementation. Will integrate.

--- fbo-dev/data/post.fs
+++ fbo-dev/data/post.fs
@@ -7,6 +7,10 @@ uniform sampler2D uTexture;
 
 void main()
 {
-	vec4 col = texture(uTexture, fragTexcoord);
+	vec4 col = vec4(0.0f);
+	for(int oy = -3; oy <= 3; oy++)
+		for(int ox = -3; ox <= 3; ox++)
+			col += textureOffset(uTexture, fragTexcoord, ivec2(ox,oy));
+	col /= 49.0;
 	outColor = vec4(1.0 - col.rgb, col.a);
 }
--- fbo-dev/fbo.h
+++ fbo-dev/fbo.h
@@ -2,16 +2,16 @@
 #define _H_FBODEV_H_
 
 #include <GL/glew.h>
+#include <set>
 
 class fbo {
 	private: 
-		struct attachment_t {
-			GLuint id;
-			GLenum type;
-		};
+		typedef std::pair<GLenum, GLuint> attachment_t;
 
 	private:
 		GLuint m_Handle = 0;
+		GLenum m_BindType = GL_FRAMEBUFFER;
+		std::set<attachment_t> m_Attachments;
 
 		void create();
 
@@ -22,7 +22,7 @@ class fbo {
 		void 	bind(GLenum type = GL_FRAMEBUFFER);
 
 		void	attach_texture(GLenum attachment_point, GLenum texture_type, GLuint texture, GLint texture_level, bool own = 1);
-		void 	attach_rbo(GLenum attachment_point, );
+		void 	attach_rbo(GLenum attachment_point, GLuint rbo, bool own = 1);
 
 		bool	free();
 };
